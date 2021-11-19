d5b2e2309e39ae06ad6444bb12204af79a91eae9
MAUPAS	Yohan <y6maupas@CROSS.ENIB.FR>
2021-11-19 17:13:39 +0100

    Initial commit

---
+++ Assets/MovePlayer.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovePlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    public float speed = 0.005f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            this.GetComponent<Rigidbody>().AddTorque(speed, 0, 0);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            this.GetComponent<Rigidbody>().AddTorque(-speed, 0, 0);
+        }
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            this.GetComponent<Rigidbody>().AddTorque(0, 0, -speed);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            this.GetComponent<Rigidbody>().AddTorque(0, 0, speed);
+        }
+    }
+}

44df271a235abb47559c0f90964030070c496ca1
Maël Donnart <mael.donnart1@gmail.com>
2021-11-21 20:29:40 +0100

    Created main menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance;
+    private float difficulty;
+    private float volume;
+
+    private void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public float GetDifficulty()
+    {
+        return this.difficulty;
+    }
+
+    public void SetDifficulty()
+    {
+        this.difficulty = gameObject.GetComponent<Slider>().value;
+    }
+
+    public void SetVolume()
+    {
+        this.volume = gameObject.GetComponent<Slider>().value;
+    }
+}
